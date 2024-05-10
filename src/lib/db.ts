872f52e907244ade4dbffb248d8b806bcb40df89
Possawat Suksai <markkung08@gmail.com>
2024-05-10 19:16:20 +0700

    feat: add student query

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,19 @@
 import { drizzle } from "drizzle-orm/postgres-js"
+import { eq } from "drizzle-orm"
 import postgres from "postgres"
+
 import * as schema from "../schema"
 
 const queryClient = postgres(process.env.DATABASE_URL as string)
 
 export const db = drizzle(queryClient, { schema })
+
+export const getUniqueStudent = async (id: number) => {
+  const student = await db
+    .select()
+    .from(schema.StudentsTable)
+    .where(eq(schema.StudentsTable.id, id))
+    .limit(1)
+
+  return student
+}
